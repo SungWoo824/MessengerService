000a13a30f5d4d750933618dcb064ca751cb39fa
SungWoo824 <scody1219@gmail.com>
2021-08-06 18:19:13 +0900

    add : index page

---
+++ messengerfront/src/component/MainIndex.js
@@ -0,0 +1,82 @@
+import React from "react";
+
+function MainIndex(){
+    return (
+        <div className="circle-main-content">
+            <div className="main-bg">
+
+                <div id="fullpage">
+                    <div className="section" data-anchor="test1">
+                        <image className="home-img" src=""/>
+                            <image className="home-img2" src=""/>
+                                <div className="main-title">
+                                    <div>
+                                        <span>효과적인 팀워크, 가벼워진 업무</span>
+                                        <p>이메일이 필요없는 간편한 소통과 파일 공유, 빠른 피드백 확인까지.
+                                            <br/>바라던 기능들을 모두 담아
+                                                <br/>업무에 최적화된 커뮤니케이션이 가능해집니다.</p>
+                                    </div>
+                                    <div className="main-btn">
+                                        <div className="main-btn-bg">
+                                            <a href="member/signup">무료 체험 시작</a><br/><br/>
+                                        </div>
+                                    </div>
+                                </div>
+                    </div>
+                    <div className="section" data-anchor="test2">
+                        <div className="about-title">
+                            <span>ABOUT CIRCLE</span>
+                            <p>우리는 팀 커뮤니케이션을 위한 새롭고 혁신적인 방법을 제공하고자 합니다.<br/>
+                                그룹중심의 메신저 기반으로 빠른 소통과 간편한 프로젝트 관리가 가능해집니다.<br/>
+                                써클(circle)을 통해 함께 일하는 것이 어떻게 달라질 수 있는지 경험해 보세요.
+                            </p>
+                        </div>
+                        <image className="home-img3" src=""/>
+
+                    </div>
+                    <div className="section" data-anchor="test3">
+                        <div className="pay-title">
+                            <span>요금 안내</span>
+                            <p>써클을 함께 사용할 멤버는 몇 명인가요?</p>
+                        </div>
+                        <div className="pay-info">
+                            <div className="pay-box">
+                                <div className="pay-one">
+                                    <span>1개월권</span>
+                                    <div className="pay-line"></div>
+                                    <div className="pay-p">
+                                        <p>개당</p>
+                                        <p className="price">5000</p>
+                                        <p className="price-one">￦</p>
+                                    </div>
+                                </div>
+                                <div className="pay-one">
+                                    <span>6개월권</span>
+                                    <div className="pay-line"></div>
+                                    <div className="pay-p">
+                                        <p>개당</p>
+                                        <p className="price">27000</p>
+                                        <p className="price-one">￦</p>
+                                    </div>
+                                </div>
+                                <div className="pay-one pay-last">
+                                    <span>1년권</span>
+                                    <div className="pay-line"></div>
+                                    <div className="pay-p">
+                                        <p>개당</p>
+                                        <p className="price">48000</p>
+                                        <p className="price-one">￦</p>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    );
+}
+
+export default MainIndex;
--- messengerfront/src/component/MainNavigation.js
+++ messengerfront/src/component/MainNavigation.js
@@ -1,7 +1,6 @@
 import React from "react";
-import {Link} from "react-router-dom";
 
-function MainNavigation() {
+function MainNavigation({token}) {
     return (
         <div className="circle-header">
 
@@ -9,7 +8,7 @@ function MainNavigation() {
                 <div className="header-logo">
                     <div className="mainlogo">
                         <a href="">
-                            <img src=""/>
+                            <image src=""/>
                         </a>
                     </div>
                 </div>
@@ -18,26 +17,24 @@ function MainNavigation() {
                         <li><a href="#hash1" data-menuanchor="test1">무료체험하기</a></li>
                         <li><a href="#hash2" data-menuanchor="test2">CIRCLE소개</a></li>
                         <li><a href="#hash3" data-menuanchor="test3">요금안내</a></li>
-                        <c:choose>
-                            <c:when test="${not empty member_email}">
-                                <li><a href="${pageContext.request.contextPath}/member/mypage">내정보</a></li>
-                            </c:when>
-                        </c:choose>
+                        {
+                            {token} !== ''
+                            ? <li><a href="">내정보</a></li>
+                            : null
+                        }
                     </ul>
                     <ul className="gnb-menu">
-
-                        <c:choose>
-                            <c:when test="${not empty member_email}">
-                                <li className="logout-btn"><a
-                                    href="/member/signout">로그아웃</a></li>
-                            </c:when>
-                            <c:otherwise>
-                                <li className="login-btn"><a
-                                    href="/member/signin">로그인</a></li>
-                            </c:otherwise>
-                        </c:choose>
-
-
+                        {
+                            {token} !== ''
+                                ?
+                                    <li className="logout-btn">
+                                        <a href="/member/signout">로그아웃</a>
+                                    </li>
+                                :
+                                    <li className="login-btn">
+                                        <a href="/member/signin">로그인</a>
+                                    </li>
+                        }
                     </ul>
                 </div>
             </div>
