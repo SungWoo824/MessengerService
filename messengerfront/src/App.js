1caa8e33807f73966bfdd507e129c80ec6245b13
SungWoo824 <scody1219@gmail.com>
2021-08-08 16:27:33 +0900

    add : start signin page front

--- messengerfront/src/App.js
+++ messengerfront/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { HashRouter, Route} from "react-router-dom";
 import Home from "./routes/Main";
+import SignIn from "./routes/SignIn";
 import './App.css';
 
 function App() {
   return (
     <HashRouter>
         <Route path="/" component={Home}/>
+        <Route path="/member/signin" component={SignIn}/>
     </HashRouter>
   );
 }
---
+++ messengerfront/src/routes/SignIn.js
@@ -0,0 +1,19 @@
+import React from "react";
+import MainNavigation from "../component/MainNavigation";
+import SignInComponent from "../component/SignInComponent";
+
+class SignIn extends React.Component{
+    state = {
+        token : localStorage.getItem("token")??''
+    }
+    render() {
+        return (
+            <div>
+                <MainNavigation state={this.state}/>
+                <SignInComponent/>
+            </div>
+        );
+    }
+}
+
+export default SignIn;
