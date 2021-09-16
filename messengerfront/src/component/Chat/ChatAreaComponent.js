dde3a2790f66dc96f98cd9b28bf015c28a32380b
SungWoo824 <scody1219@gmail.com>
2021-09-16 20:34:15 +0900

    add : chatContent component

--- messengerfront/src/component/Chat/ChatAreaComponent.js
+++ messengerfront/src/component/Chat/ChatAreaComponent.js
@@ -27,6 +27,14 @@ function ChatAreaComponent(){
     };
     return(
         <div>
+            <SockJsClient
+                url={"http://localhost:6002/my-chat/"}
+                topics={["/topic/group"]}
+                onConnect={console.log("connected!")}
+                onDisconnect={console.log("disconnected!")}
+                onMessage={(msg) => onMessageReceived(msg)}
+                debug={false}
+            />
             <div className="chat-wrap">
                 <div className="chat-aside">
                     <div className="chat-menu-bar">
@@ -38,69 +46,7 @@ function ChatAreaComponent(){
 
                                 <div className="message" style="text-align: left">
                                     <div id="chat-content"  style="display: flex; flex-direction: column-reverse;">
-                                        <c:forEach items="${topicChatList}" var="chatVo">
-                                            <c:choose>
-                                                <c:when test="${chatVo.chat_status==2}">
-                                                    <div className="msg-wrap">
-                                                        <div className="msg-profile">
-                                                            <img id="member-profile-img" src=''/>
-                                                        </div>
 
-                                                        <div className="msg-con">
-                                                            <span>chatVo.member_name</span>
-                                                            <p className="msg">chatVo.chat_content</p>
-                                                        </div>
-                                                    </div>
-                                                </c:when>
-
-                                                <c:when test="${chatVo.chat_status==4}">
-
-                                                    <div className="msg-wrap">
-                                                        <div className="msg-profile">
-                                                            <img id="member-profile-img" src=''/>
-                                                        </div>
-
-                                                        <div className="msg-con">
-                                                            <span>chatVo.member_name</span>
-                                                            <div className="card border-primary mb-3 admin-card" style="width: 20rem; height: 15rem;">
-                                                                <div className="card-body admin-card-body">
-                                                                    <c:choose>
-                                                                        <c:when test="${fn:startsWith(chatVo.chat_file_type,'text')}">
-                                                                            <div>
-                                                                                <a href="filedownload?chat_file_no=${chatVo.chat_no}">
-                                                                                    <img id="chat_dummy" src=""/>
-                                                                                </a>
-                                                                            </div>
-                                                                        </c:when>
-                                                                        <c:when test="${fn:startsWith(chatVo.chat_file_type,'image')}">
-                                                                            <div>
-                                                                                <a href="filedownload?chat_file_no=${chatVo.chat_no}">
-                                                                                    <img id="chat_dummy" src=''/>
-                                                                                </a>
-                                                                            </div>
-                                                                        </c:when>
-                                                                        <c:otherwise>
-                                                                            <div>
-                                                                                <a href="filedownload?chat_file_no=${chatVo.chat_no}">
-                                                                                    <img id="chat_dummy" src=""/>
-                                                                                </a>
-                                                                            </div>
-                                                                        </c:otherwise>
-
-                                                                    </c:choose>
-                                                                    <p className="card-text">
-                                                                        <a href="#">
-
-                                                                        </a>
-                                                                    </p>
-                                                                </div>
-                                                                <div className="card-header">chatVo.chat_content</div>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </c:when>
-                                            </c:choose>
-                                        </c:forEach>
 
                                     </div>
                                 </div>
---
+++ messengerfront/src/component/Chat/ChatContentComponent.js
@@ -0,0 +1,61 @@
+import React from "react";
+
+function ChatContentComponent({id, chatNo, memberName, chatContent, chatFileType, chatStatus}) {
+    return (
+        <div>
+            {chatStatus===2
+            ?
+            (<div className="msg-wrap">
+                <div className="msg-profile">
+                    <img id="member-profile-img" src='' alt="profileImg"/>
+                </div>
+
+                <div className="msg-con">
+                    <span>{memberName}</span>
+                    <p className="msg">{chatContent}</p>
+                </div>
+            </div>):
+            (<div className="msg-wrap">
+                <div className="msg-profile">
+                    <img id="member-profile-img" src='' alt="profileImg"/>
+                </div>
+
+                <div className="msg-con">
+                    <span>{memberName}</span>
+                    <div className="card border-primary mb-3 admin-card" style="width: 20rem; height: 15rem;">
+                        <div className="card-body admin-card-body">
+                            {chatFileType === "text" ?
+                            (<div>
+                                <a href={"filedownload?chat_file_no="+{chatNo}}>
+                                    <img id="chat_dummy" src="" alt="textFileImage"/>
+                                </a>
+                            </div>):
+                            (chatFileType === "image" ?(
+                            <div>
+                                <a href={"filedownload?chat_file_no="+{chatNo}}>
+                                    <img id="chat_dummy" src='' alt="imageFileImage"/>
+                                </a>
+                            </div>
+                            ):(
+                            <div>
+                                <a href={"filedownload?chat_file_no="+{chatNo}}>
+                                    <img id="chat_dummy" src="" alt="otherFileImage"/>
+                                </a>
+                            </div>
+                            ))
+                            }
+                            <p className="card-text">
+                                <a href="#">
+
+                                </a>
+                            </p>
+                        </div>
+                        <div className="card-header">{chatContent}</div>
+                    </div>
+                </div>
+            </div>)
+
+            }
+        </div>
+    )
+}
