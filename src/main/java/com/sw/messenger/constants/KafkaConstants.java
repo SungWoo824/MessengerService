8ce7509f7aee1169a03249155beb846f397aabd6
SungWoo824 <scody1219@gmail.com>
2021-09-20 19:48:30 +0900

    fix : kafka replication

--- src/main/java/com/sw/messenger/constants/KafkaConstants.java
+++ src/main/java/com/sw/messenger/constants/KafkaConstants.java
@@ -2,6 +2,6 @@ package com.sw.messenger.constants;
 
 public class KafkaConstants {
     public static final String KAFKA_TOPIC = "kafka-chat";
-    public static final String GROUP_ID = "foo";
+    public static final String GROUP_ID = "test-consumer-group";
     public static final String KAFKA_BROKER = "localhost:9092";
 }
