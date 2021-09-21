7fef91440936f803145a720678d67ebb74a847e7
SungWoo824 <scody1219@gmail.com>
2021-09-21 18:54:09 +0900

    fix : kafka server

--- src/main/java/com/sw/messenger/constants/KafkaConstants.java
+++ src/main/java/com/sw/messenger/constants/KafkaConstants.java
@@ -3,5 +3,5 @@ package com.sw.messenger.constants;
 public class KafkaConstants {
     public static final String KAFKA_TOPIC = "kafka-chat";
     public static final String GROUP_ID = "test-consumer-group";
-    public static final String KAFKA_BROKER = "localhost:9092";
+    public static final String KAFKA_BROKER = "localhost:9094";
 }
