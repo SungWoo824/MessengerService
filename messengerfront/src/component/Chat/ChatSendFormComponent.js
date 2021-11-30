ce4edfdd7499cd78500f319650d1c446b9274e9b
SungWoo824 <scody1219@gmail.com>
2021-11-30 23:20:45 +0900

    add : chat send form

--- messengerfront/src/component/Chat/ChatSendFormComponent.js
+++ messengerfront/src/component/Chat/ChatSendFormComponent.js
@@ -1,15 +1,30 @@
-import React from "react";
-import Axios from "axios";
+import React, {useState} from "react";
+import chatApi from "../../lib/ChatApi";
+import authentication from "../../lib/Authentication";
 
 function ChatSendFormComponent() {
+    const [inputMessage, setInputMessage] = useState('');
+    const handleInputMessage = (e) => {
+        setInputMessage(e.target.value);
+    }
+    const handleMessageSubmit = (msg) => {
+        chatApi.sendMessage(authentication.getLoggedInUserName(),msg)
+            .then((res) => {
+                console.log("sent", res);
+            }).catch((e) => {
+                console.log(e);
+        })
+    }
     return (
         <div className="chat-send-content">
-            <div className="chat-send-text">
-                <input id="dropzone" className="form-control user-input" type="text" placeholder="메시지를 입력하세요" onChange={}/>
-            </div>
-            <div className="chat-send-button">
-                <button type="submit" className="btn btn-lg btn-primary send-btn">전송</button>
-            </div>
+            <form onSubmit={handleMessageSubmit}>
+                <div className="chat-send-text">
+                    <input id="dropzone" className="form-control user-input" type="text" placeholder="메시지를 입력하세요" onChange={handleInputMessage}/>
+                </div>
+                <div className="chat-send-button">
+                    <button type="submit" className="btn btn-lg btn-primary send-btn">전송</button>
+                </div>
+            </form>
         </div>
     );
 }
